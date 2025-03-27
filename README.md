61b303e5b84c2846224cb2c929a49d785fdc2b4a
Ankita Sood <ankitasood2000@gmail.com>
2025-03-27 09:06:15 +0530

    Lecture 2 : Igniting our app

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Namaste React Episode 1 - Inception
+
+
+
+# Parcel
+- Local build production ready
+- Local server
+- HMR - hot module replacement
+- File watching algorithm - written c++
+- Faster build by caching
+- Image optimisation
+- minification 
+- bundling
+- compress the file
+- consistent hashing
+- code splitting
+- differential bundling - to support older browsers
+- diagnostic / error handling 
+- host in https
+- tree shaking - remove unused code 
+- different dev and production build
+
+Issues faced - 
+1. npm run build with parcel throws an error
+Solution: https://stackoverflow.com/questions/71053013/npm-run-build-with-parcel-throws-an-error
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // Mostly in react it is a nested so how do one create for eg lets say
 //
 //  <div id="parent">
@@ -12,19 +14,25 @@
 //  </div>
 // React Element(Object) => HTML element(Browser understands)
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "hello from anki"),
-    React.createElement("h2", {}, "hello from bni"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "hello from ANKI"),
-    React.createElement("h2", {}, "hello from BNI"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "hello from anki"),
+//     React.createElement("h2", {}, "hello from bni"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "hello from ANKI"),
+//     React.createElement("h2", {}, "hello from BNI"),
+//   ]),
+// ]);
+
+const newWithNoChildren = React.createElement(
+  "div",
+  { id: "parent" },
+  "Hello world"
+);
 
 // JSX
 // const heading = React.createElement("h1", { id: "heading" }, "hello anki");
 // console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(newWithNoChildren);
