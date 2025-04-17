1058b06c309c0762beefc0b5832fa37139d0bd7a
ankisood <ankitasood2000@gmail.com>
2025-04-17 23:32:46 +0530

    Test

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,5 +2,5 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 
 test("should load contact us page", () => {
-  render(<Contact />);
+  // render(<Contact />);
 });
